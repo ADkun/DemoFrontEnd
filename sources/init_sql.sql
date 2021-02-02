c87ee9a39ce15b87ec8a66655396ce9e3ac473e7
ADkun <adgnn@foxmail.com>
2021-02-02 08:04:14 +0800

    Add sources

---
+++ sources/init_sql.sql
@@ -0,0 +1,39 @@
+create database bbb;
+
+use bbb;
+
+CREATE TABLE `teacher` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `age` int DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8;
+
+insert into teacher
+(age) values (28);
+
+insert into teacher
+(age) values (38);
+
+insert into teacher
+(age) values (48);
+
+insert into teacher
+(age) values (58);
+
+insert into teacher
+(age) values (68);
+
+insert into teacher
+(age) values (78);
+
+insert into teacher
+(age) values (88);
+
+insert into teacher
+(age) values (98);
+
+insert into teacher
+(age) values (108);
+
+insert into teacher
+(age) values (118);
